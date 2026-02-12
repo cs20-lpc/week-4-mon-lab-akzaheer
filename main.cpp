59a3c29a9839506b9dcd2702d18b7c2d56034c0c
ayaanzaheer <ayaanzaheer@gmail.com>
2026-02-11 18:06:30 -0800

    Update files

--- main.cpp
+++ main.cpp
@@ -1,100 +1,33 @@
-#include "LinkedList.hpp"
+#include <iostream>
 #include <string>
+#include "LinkedList.hpp"
+using namespace std;
+    // edited this
+int main(){
 
-int main() {
-    // call the constructor
-    LinkedList<string> myList;
+    LinkedList<int> intList;                   // int testing
+    cout << "is it empty (1 if true and 0 if false): " << intList.isEmpty() << endl;            // print results
+    intList.insert(0,67);                   // insert 10 at 0
+    intList.insert(1,41);               /// insert 20 at 1
+    cout << "int list after adding stuff: " << intList << endl;            // print results
+    intList.remove(0);                  // remove pos 0
+    cout << "int list after removing 0 pos : " << intList << endl;            // print results
+    
+    //    intList.remove(5);                  // remove pos 5
+    //    cout << "int list after removing 0 pos : " << intList << endl;            // print results or error
 
-    // add elements to the list
-    myList.append("Heart");
-    myList.insert(0, "Your");
-    myList.insert(0, "May");
+    LinkedList<int> intCopy(intList);       // make new list and copy old onto new
+    cout << "Copied list : " << intCopy << endl;            // print results
 
-    // try replacing as an invalid operation
-    try {
-        myList.replace(-3, "?");
-    }
-    catch (string& e) {
-        cerr << e << endl;
-    }
+    LinkedList<string> stringList;                   // string testing
+    cout << "is it empty (1 if true and 0 if false): " << stringList.isEmpty() << endl;   // print results
+    stringList.insert(0,"Lebron");               // insert at 0
+    stringList.insert(1,"James");               // insert at 1
+    cout << "string list after adding stuff: " << stringList << endl;   // print results
+    stringList.remove(0);                       // remove pos 0
+    cout << "string list after removing 0 pos: " << stringList << endl; // print results
+    LinkedList<string> stringCopy(stringList);  // copy constructor
+    cout << "Copied string list: " << stringCopy << endl;   // print results
 
-    // add another element
-    myList.append("Citrus");
-
-    // try inserting as an invalid operation
-    try {
-        myList.insert(5, "kweh");
-    }
-    catch (string& e) {
-        cerr << e << endl;
-    }
-
-    // remove an element
-    myList.remove(3);
-
-    // display the state of the list
-    cout << myList;
-
-    // get the second element and display it
-    string word = myList.getElement(2);
-    cout << "Element 2: " << word << endl;
-
-    // create a second list using the copy constructor
-    LinkedList<string> secondList = myList;
-
-    // modify the second list and display its state
-    secondList.append("Beat");
-    secondList.replace(2, "Drums");
-    cout << secondList;
-
-    // try removing an element as an invalid operation
-    try {
-        secondList.remove(4);
-    }
-    catch (string& e) {
-        cerr << e << endl;
-    }
-
-    // create a third list using the default constructor (max size of 100)
-    LinkedList<string> thirdList;
-
-    // display the state of the list, should be empty
-    cout << thirdList;
-
-    // copy the first list to the third list using assignment overload
-    thirdList = myList;
-
-    // display the length and max size, should match the first list
-    cout << "thirdList length: " << thirdList.getLength() << endl;
-
-    // add elements to the list
-    thirdList.append("Be A");
-    thirdList.append("Yellow");
-    thirdList.append("Banana");
-
-    // display the state of the list
-    cout << thirdList;
-
-    // try getting elements as an invalid operation
-    try {
-        cout << thirdList.getElement(-7) << endl;
-    }
-    catch (string& e) {
-        cerr << e << endl;
-    }
-    try {
-        cout << thirdList.getElement(7) << endl;
-    }
-    catch (string& e) {
-        cerr << e << endl;
-    }
-
-    // clear out the list and verify it is now empty
-    myList.clear();
-    if (myList.isEmpty()) {
-        cout << "myList is empty!\n";
-    }
-
-    // terminate
     return 0;
-}
+} 
